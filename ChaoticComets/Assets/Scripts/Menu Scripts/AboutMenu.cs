f11c3d978f6af911e8eb05ab31550d3b4a6bdfe2
Cameron <cam.crosse97@outlook.com>
2020-05-29 17:36:23 +1000

    1.3 Dev: About Menu Finished
    Continuing development on v1.3 of Chaotic Comets. This commit concerns the completion of v1.3's AboutMenu.
    
    AboutMenu
    - Dev history images all updated to be consistent
    -- Frame added around the image area
    - Text slightly edited in places

--- ChaoticComets/Assets/Scripts/Menu Scripts/AboutMenu.cs
+++ ChaoticComets/Assets/Scripts/Menu Scripts/AboutMenu.cs
@@ -12,25 +12,26 @@ public class AboutMenu : MonoBehaviour {
     public int stage = 0;
 
     // UI variables
-    public Sprite dev0, dev1, dev2, dev3, dev4, dev5, dev6, devLast;
+    public Sprite devIntro, devPre1, devPre2, devPre3, devPreFinal, dev1_0, dev1_1, dev1_2, dev1_3, devUpcoming;
     public Image devImage;
     public TextMeshProUGUI devText;
     public Text devNextButtonText;
-    private readonly string devText0 = "<u>Early October 2018</u>: Chaotic Comet's development began with a simple experiment of game physics. Much of the material was placeholders.",
-        devText1 = "<u>Late October 2018</u>: The project was in a playable state. Asteroid physics, a health/power system, and a basic menu system was added.",
-        devText2 = "<u>Early November 2018</u>: Further development led to a multiplayer mode, UFO enemies, and powerups.",
-        devText3 = "<u>Late November 2018</u>: All placeholder sprites were removed, replaced with 3D assets made from scratch. This was the build used for a public design show. It was received well.",
-        devText4 = "<u>March 2019</u>: Chaotic Comets was revisited to be published online. It came with UI enhancements, difficulty options, and easier controller selection.",
-        devText5 = "<u>May 2019</u>: Chaotic Comets was updated to 1.1. Ship handling & visual effects were improved, sound effects / music were added, and plenty of bugs were fixed.",
-        devText6 = "<u>June 2019</u>: Chaotic Comets was updated to 1.2. UFO's have more interesting mechanics, saving and a shop between levels was introduced, and a lot of testing with how the UI is handled.",
-        devText7 = "<u>June 2020</u>: After a year of on-and-off dev, 1.3 was done. UFO AI improved, interactive tutorial, comprehensive controller support, and a <i>ton</i> of code review & bug fixes.",
-        devText8 = "Upcoming patches will include more features to enhance gameplay. Thank you for trying out the game!";
+    private readonly string devTextIntro = "Chaotic Comets has been in development since late 2018. Press the button below to learn more about the game's history.",
+        devTextPre1 = "<u>Early October 2018</u>: Chaotic Comet's development began with a simple experiment of game physics. Much of the material was placeholders.",
+        devTextPre2 = "<u>Late October 2018</u>: The project was in a playable state. Asteroid physics, a health/power system, and a basic menu system was added.",
+        devTextPre3 = "<u>Early November 2018</u>: Further development led to a multiplayer mode, UFO enemies, and powerups.",
+        devTextPreFinal = "<u>Late November 2018</u>: All placeholder sprites were removed, replaced with 3D assets made from scratch. This was the build used for a public design show. It was received well.",
+        devText1_0 = "<u>March 2019</u>: Chaotic Comets was revisited to be published online. It came with UI enhancements, difficulty options, and easier controller selection.",
+        devText1_1 = "<u>May 2019</u>: Chaotic Comets was updated to 1.1. Ship handling & visual effects were improved, sound effects / music were added, and plenty of bugs were fixed.",
+        devText1_2 = "<u>June 2019</u>: Chaotic Comets was updated to 1.2. UFO's have more interesting mechanics, saving and a shop between levels was introduced, and there was a lot of testing done.",
+        devText1_3 = "<u>June 2020</u>: After a year of on-and-off dev, 1.3 was done. Includes improved UFO AI, an interactive tutorial, comprehensive controller support, and a <i>ton</i> of code review & bug fixes.",
+        devTextUpcoming = "Upcoming patches will include more features to enhance gameplay. Thank you for playing the game!";
     public Button returnToMenuButton;
 
     // ----------
 
     void Start() {
-        devText.text = devText0;
+        devText.text = devTextIntro;
     }
 
     void Update() {
@@ -44,47 +45,52 @@ public class AboutMenu : MonoBehaviour {
     }
 
     public void NextStageOfDev() {
-        if (stage < 7) { stage++; }
+        if (stage < 9) { stage++; }
         else { stage = 0; }
         if (stage == 0) {
-            devImage.sprite = dev0;
-            devText.text = devText0;
+            devImage.sprite = devIntro;
+            devText.text = devTextIntro;
             devNextButtonText.text = "Learn about development >";
         }
         else if (stage == 1) {
-            devImage.sprite = dev1;
-            devText.text = devText1;
+            devImage.sprite = devPre1;
+            devText.text = devTextPre1;
             devNextButtonText.text = "More development info >";
         }
         else if (stage == 2) {
-            devImage.sprite = dev2;
-            devText.text = devText2;
+            devImage.sprite = devPre2;
+            devText.text = devTextPre2;
         }
         else if (stage == 3) {
-            devImage.sprite = dev3;
-            devText.text = devText3;
+            devImage.sprite = devPre3;
+            devText.text = devTextPre3;
         }
         else if (stage == 4) {
-            devImage.sprite = dev4;
-            devText.text = devText4;
+            devImage.sprite = devPreFinal;
+            devText.text = devTextPreFinal;
         }
         else if (stage == 5) {
-            devImage.sprite = dev5;
-            devText.text = devText5;
+            devImage.sprite = dev1_0;
+            devText.text = devText1_0;
         }
         else if (stage == 6) {
-            devImage.sprite = dev6;
-            devText.text = devText6;
+            devImage.sprite = dev1_1;
+            devText.text = devText1_1;
         }
         else if (stage == 7)
         {
-            devImage.sprite = devLast;
-            devText.text = devText7;
+            devImage.sprite = dev1_2;
+            devText.text = devText1_2;
+        }
+        else if (stage == 8)
+        {
+            devImage.sprite = dev1_3;
+            devText.text = devText1_3;
             devNextButtonText.text = "Upcoming features >";
         }
-        else if (stage == 8) {
-            devImage.sprite = devLast;
-            devText.text = devText8;
+        else if (stage == 9) {
+            devImage.sprite = devUpcoming;
+            devText.text = devTextUpcoming;
             devNextButtonText.text = "< To start of development";
         }
     }
